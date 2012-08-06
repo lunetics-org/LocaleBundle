282ff52a265743e39fa2f10897ef2d47233c5076
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-06 11:27:35 +0200

    localeGuessingManager + bump localeListener

--- Tests/EventListener/LocaleListenerTest.php
+++ Tests/EventListener/LocaleListenerTest.php
@@ -3,23 +3,42 @@
 namespace Lunetics\LocaleBundle\Tests\EventListener;
 
 use Lunetics\LocaleBundle\EventListener\LocaleListener;
+use Lunetics\LocaleBundle\LocaleGuesser\LocaleGuesserManager;
+use Lunetics\LocaleBundle\LocaleGuesser\RouterLocaleGuesser;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 class LocaleListenerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testDefaultLocaleWithoutSession()
+    public function testDefaultLocaleWithoutParams()
     {
-        $listener = new LocaleListener('fr');
+        $listener = new LocaleListener('fr', $this->getGuesserManager());
         $event = $this->getEvent($request = Request::create('/'));
 
         $listener->onKernelRequest($event);
         $this->assertEquals('fr', $request->getLocale());
     }
+    
+    public function testCustomLocaleIsSetWhenParamsExist()
+    {
+        $listener = new LocaleListener('fr', $this->getGuesserManager());
+        $event = $this->getEvent($request = Request::create('/', 'GET', array('_locale' => 'de')));
+
+        $listener->onKernelRequest($event);
+        $this->assertEquals('de', $request->getLocale());
+    }
 
     private function getEvent(Request $request)
     {
         return new GetResponseEvent($this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface'), $request, HttpKernelInterface::MASTER_REQUEST);
+    }
+    
+    private function getGuesserManager()
+    {
+        $routerGuesser = new RouterLocaleGuesser();
+        $guessingOrder = array(1 => 'router', 2 => 'browser');
+        $manager = new LocaleGuesserManager($guessingOrder, $routerGuesser);
+        return $manager;
     }
 }
