54b46dd406526f3c398d919d316c6d147990fc1a
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-06 11:27:35 +0200

    localeGuessingManager + bump localeListener

--- EventListener/LocaleListener.php
+++ EventListener/LocaleListener.php
@@ -3,20 +3,33 @@
 namespace Lunetics\LocaleBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Lunetics\LocaleBundle\LocaleGuesser\LocaleGuesserManager;
 
 class LocaleListener
 {
     private $defaultLocale;
     
-    public function __construct($defaultLocale = 'en')
+    private $guesserManager;
+    
+    public function __construct($defaultLocale = 'en', LocaleGuesserManager $guesserManager)
     {
         $this->defaultLocale = $defaultLocale;
+        $this->guesserManager = $guesserManager;
     }
     
     public function onKernelRequest(GetResponseEvent $event)
-    {
+    {        
+        if($event->getRequestType() !== HttpKernelInterface::MASTER_REQUEST) {
+            return;
+        }
+        
         $request = $event->getRequest();
-
+        $manager = $this->guesserManager;
+        if($locale = $manager->runLocaleGuessing($request)){
+            $request->setDefaultLocale($locale);
+            return;
+        }
         $request->setDefaultLocale($this->defaultLocale);
     }
 }
---
+++ Tests/LocaleGuesser/LocaleGuesserManagerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Lunetics\LocaleBundle\Tests\LocaleGuesser;
+
+use Lunetics\LocaleBundle\LocaleGuesser\RouterLocaleGuesser;
+use Lunetics\LocaleBundle\LocaleGuesser\LocaleGuesserManager;
+use Lunetics\LocaleBundle\LocaleGuesser\LocaleGuesserInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class LocaleGuesserManagerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGuesserNameIsMappedToService()
+    {
+        $routerGuesser = new RouterLocaleGuesser();
+        $order = array(0 => 'router', 1 => 'browser');
+        $manager = new LocaleGuesserManager($order, $routerGuesser);
+        $guessersMap = $manager->getGuessingServices();
+        $this->assertTrue($guessersMap['router'] instanceof LocaleGuesserInterface);
+    }
+    
+    public function testLocaleIsIdentifiedByTheRouterGuessingService()
+    {
+        $request = $this->getRequestWithLocaleQuery('fr');
+        $routerGuesser = new RouterLocaleGuesser();
+        $order = array(0 => 'router', 1 => 'browser');
+        $manager = new LocaleGuesserManager($order, $routerGuesser);
+        $guessing = $manager->runLocaleGuessing($request);
+        $this->assertEquals('fr', $guessing);
+    }
+    
+    public function testLocaleIsNotIdentifiedIfNoQueryParamsExist()
+    {
+        $request = $this->getRequestWithoutLocaleQuery();
+        $routerGuesser = new RouterLocaleGuesser();
+        $order = array(0 => 'router', 1 => 'browser');
+        $manager = new LocaleGuesserManager($order, $routerGuesser);
+        $guessing = $manager->runLocaleGuessing($request);
+        $this->assertEquals(false, $guessing);
+    }
+    
+    private function getRequestWithLocaleQuery($locale = 'en')
+    {
+        $request = Request::create('/hello-world', 'GET', array('_locale' => $locale));
+        return $request;
+    }
+    
+    private function getRequestWithoutLocaleQuery()
+    {
+        $request = Request::create('/hello-world', 'GET');
+        return $request;
+    }
+}
