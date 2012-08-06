b952615e6bf1fcb80c099c698ac6a74cad87a923
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-06 15:35:10 +0200

    added localeCookieClass

---
+++ Cookie/LocaleCookie.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the LuneticsLocaleBundle package.
+ * 
+ * <https://github.com/lunetics/LocaleBundle/>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that is distributed with this source code.
+ */
+namespace Lunetics\LocaleBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\Cookie;
+
+/**
+ * @author Christophe Willemsen <willemsen.christophe@gmail.com/>
+ */
+class LocaleCookie extends Cookie
+{
+    
+}
