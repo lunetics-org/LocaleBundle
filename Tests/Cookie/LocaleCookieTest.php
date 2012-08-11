4527e79fbe163582e85eaf5c589ef2906778d466
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-08-11 02:02:34 +0200

    fixed time() function

--- Tests/Cookie/LocaleCookieTest.php
+++ Tests/Cookie/LocaleCookieTest.php
@@ -27,8 +27,8 @@ class LocaleCookieTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($cookie->isHttpOnly());
         $this->assertFalse($cookie->isSecure());
         print_r($cookie->getExpiresTime());
-        $this->assertTrue($cookie->getExpiresTime() > mktime());
-        $this->assertTrue($cookie->getExpiresTime() < mktime() + 86400);
+        $this->assertTrue($cookie->getExpiresTime() > time());
+        $this->assertTrue($cookie->getExpiresTime() < time() + 86400);
     }
 
     public function testCookieExpiresDateTime()
