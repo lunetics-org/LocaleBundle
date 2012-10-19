782a5a9fa3aef95d27750e12e14e9dcdf07744bf
Matthias Breddin <mb@lunetics.com>
2012-10-19 05:28:58 +0200

    Optimized the Browserlocaleguesser Make ext-intl optional and build in a fallback method + tests Cleaned up dist phpunit config file

--- LocaleGuesser/BrowserLocaleGuesser.php
+++ LocaleGuesser/BrowserLocaleGuesser.php
@@ -12,6 +12,8 @@ namespace Lunetics\LocaleBundle\LocaleGuesser;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
+ * Locale Guesser for detecing the locale from the browser Accept-language string
+ *
  * @author Matthias Breddin <mb@lunetics.com>
  * @author Christophe Willemsen <willemsen.christophe@gmail.com>
  */
@@ -23,6 +25,8 @@ class BrowserLocaleGuesser implements LocaleGuesserInterface
 
     private $identifiedLocale;
 
+    private $intlExtension;
+
     /**
      * Constructor
      *
@@ -33,6 +37,7 @@ class BrowserLocaleGuesser implements LocaleGuesserInterface
     {
         $this->defaultLocale = $defaultLocale;
         $this->allowedLocales = $allowedLocales;
+        $this->intlExtension = extension_loaded('intl');
     }
 
     /**
@@ -44,29 +49,58 @@ class BrowserLocaleGuesser implements LocaleGuesserInterface
      */
     public function guessLocale(Request $request)
     {
-        // Get the Preferred Language from the Browser
-        $preferredLanguage = $request->getPreferredLanguage();
-        $providedLanguages = $request->getLanguages();
+        // Get the preferred locale from the Browser.
+        $preferredLocale = $request->getPreferredLanguage();
+        $availableLocales = $request->getLanguages();
+        $allowedLocales = $this->allowedLocales;
 
-        if (!$preferredLanguage OR count($providedLanguages) === 0) {
+
+        if (!$preferredLocale OR count($availableLocales) === 0) {
             return false;
         }
 
-        if (!in_array(\Locale::getPrimaryLanguage($preferredLanguage), $this->allowedLocales)) {
-            $availableLanguages = $this->allowedLocales;
-            $map = function($v) use ($availableLanguages) {
-                if (in_array(\Locale::getPrimaryLanguage($v), $availableLanguages)) {
+        // If the preferred primary locale is allowed, return the locale.
+        if (in_array($preferredLocale, $allowedLocales)) {
+            $this->identifiedLocale = $preferredLocale;
+
+            return true;
+        }
+
+        if ($this->intlExtension) {
+            $primaryLanguage = \Locale::getPrimaryLanguage($preferredLocale);
+        } else {
+            $primaryLanguage = $this->getPrimaryLanguage($preferredLocale);
+        }
+
+        if (!in_array($primaryLanguage, $allowedLocales)) {
+
+            // Try to find a full locale (Language + country)
+            $matchLocale = function ($v) use ($allowedLocales) {
+                if (in_array($v, $allowedLocales)) {
                     return true;
                 }
+
+                return false;
             };
-            $result = array_values(array_filter($providedLanguages, $map));
+
+            $result = array_values(array_filter($availableLocales, $matchLocale));
+
+            if (!empty($result)) {
+                $this->identifiedLocale = $result[0];
+
+                return true;
+            }
+
+            // Try to find a language
+            $availableLanguages = $this->compileLanguageArray($availableLocales);
+            $result = array_values(array_filter($availableLanguages, $matchLocale));
             if (!empty($result)) {
                 $this->identifiedLocale = $result[0];
 
                 return true;
             }
         } else {
-            $this->identifiedLocale = $preferredLanguage;
+            $this->identifiedLocale = $preferredLocale;
 
             return true;
         }
@@ -74,6 +108,50 @@ class BrowserLocaleGuesser implements LocaleGuesserInterface
         return false;
     }
 
+    /**
+     * Fallback function for fetching the primary language, if no intl extension is installed.
+     *
+     * @param string $locale
+     *
+     * @return null|string
+     */
+    private function getPrimaryLanguage($locale)
+    {
+        $primaryLanguage = substr($locale, 0, 2);
+        if (preg_match('/[a-z]{2}/', $primaryLanguage)) {
+            return $primaryLanguage;
+        }
+
+        return null;
+    }
+
+    /**
+     * Compiles a list of all available languages from a locale list
+     *
+     * @param array $availableLocales
+     *
+     * @return array
+     */
+    private function compileLanguageArray(array $availableLocales = array())
+    {
+        $providedLanguages = array();
+        foreach ($availableLocales as $locale) {
+            if ($this->intlExtension) {
+                $language = $this->getPrimaryLanguage($locale);
+            } else {
+                $language = \Locale::getPrimaryLanguage($locale);
+            }
+            if ($language) {
+                $providedLanguages[] = $language;
+            }
+        }
+
+        return array_unique($providedLanguages);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function getIdentifiedLocale()
     {
         if (null === $this->identifiedLocale) {
--- Tests/LocaleGuesser/BrowserLocaleGuesserTest.php
+++ Tests/LocaleGuesser/BrowserLocaleGuesserTest.php
@@ -5,8 +5,6 @@ namespace Lunetics\LocaleBundle\Tests\LocaleGuesser;
 use Lunetics\LocaleBundle\LocaleGuesser\BrowserLocaleGuesser;
 use Lunetics\LocaleBundle\LocaleGuesser\LocaleGuesserInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 class BrowserLocaleGuesserTest extends \PHPUnit_Framework_TestCase
 {
@@ -31,6 +29,18 @@ class BrowserLocaleGuesserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('fr_FR', $guesser->getIdentifiedLocale());
     }
 
+    public function testLocaleIsIdentifiedFromBrowserTestFallbackForNoIntlExtension()
+    {
+        $request = $this->getRequestWithBrowserPreferences();
+        $guesser = $this->getGuesser();
+        $reflectionClass = new \ReflectionClass($guesser);
+        $property = $reflectionClass->getProperty('intlExtension');
+        $property->setAccessible(true);
+        $property->setValue($guesser, false);
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_FR', $guesser->getIdentifiedLocale());
+    }
+
     public function testLocaleIsNotIdentifiedIsBrowserPreferencesIsEmpty()
     {
         $request = $this->getRequestWithEmptyBrowserPreferences();
@@ -39,36 +49,114 @@ class BrowserLocaleGuesserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(false, $guesser->getIdentifiedLocale());
     }
 
-    private function getGuesser($defaultLocale = 'en', $allowedLocales = array('en','fr','de'))
+    public function testLocaleIsNotIdentifiedIsBrowserPreferencesIsEmptyTestFallbackForNoIntlExtension()
+    {
+        $request = $this->getRequestWithEmptyBrowserPreferences();
+        $guesser = $this->getGuesser();
+        $reflectionClass = new \ReflectionClass($guesser);
+        $property = $reflectionClass->getProperty('intlExtension');
+        $property->setAccessible(true);
+        $property->setValue($guesser, false);
+        $guesser->guessLocale($request);
+        $this->assertEquals(false, $guesser->getIdentifiedLocale());
+    }
+
+    public function testRestrictionWithLocalesOnly()
+    {
+        $request = $this->getRequestWithBrowserPreferences();
+        $guesser = $this->getGuesser('en', array('fr_FR', 'en_US'));
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_FR', $guesser->getIdentifiedLocale());
+    }
+
+    public function testRestrictionWithLocalesOnlyNotIdentified()
+    {
+        $request = $this->getRequestWithBrowserPreferences();
+        $guesser = $this->getGuesser('en', array('fr_CH', 'en_GB'));
+        $guesser->guessLocale($request);
+        $this->assertFalse($guesser->getIdentifiedLocale());
+    }
+
+    public function testReturnCorrectLocaleForLanguage()
+    {
+        $request = $this->getRequestWithBrowserPreferencesMultipleLangLocales();
+        $guesser = $this->getGuesser('en', array('fr_FR', 'en_US'));
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_FR', $guesser->getIdentifiedLocale());
+    }
+
+    public function testEnsureCorrectLocaleForAllowedLocales()
+    {
+        $request = $this->getRequestWithBrowserPreferencesMultipleLangLocales();
+
+        $guesser = $this->getGuesser('en', array('de', 'en_GB'));
+        $guesser->guessLocale($request);
+        $this->assertEquals('en_GB', $guesser->getIdentifiedLocale());
+
+        $guesser = $this->getGuesser('en', array('fr', 'en_GB'));
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_CH', $guesser->getIdentifiedLocale());
+
+        $guesser = $this->getGuesser('en', array('en_GB', 'fr'));
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_CH', $guesser->getIdentifiedLocale());
+
+        $guesser = $this->getGuesser('en', array('fr_FR', 'en'));
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_FR', $guesser->getIdentifiedLocale());
+
+        $guesser = $this->getGuesser('en', array('en', 'fr_FR',));
+        $guesser->guessLocale($request);
+        $this->assertEquals('fr_FR', $guesser->getIdentifiedLocale());
+    }
+
+    public function testLocaleIsNotIdentifiedIsNoMatchedLanguage()
+    {
+        $request = $this->getRequestWithBrowserPreferences();
+        $guesser = $this->getGuesser('en', array('ar', 'es'));
+        $guesser->guessLocale($request);
+        $this->assertFalse($guesser->getIdentifiedLocale());
+    }
+
+    public function testLocaleIsNotIdentifiedIsNoMatchedLanguageTestFallbackForNoIntlExtension()
+    {
+        $request = $this->getRequestWithBrowserPreferences();
+        $guesser = $this->getGuesser('en', array('ar', 'es'));
+        $reflectionClass = new \ReflectionClass($guesser);
+        $property = $reflectionClass->getProperty('intlExtension');
+        $property->setAccessible(true);
+        $property->setValue($guesser, false);
+        $guesser->guessLocale($request);
+        $this->assertFalse($guesser->getIdentifiedLocale());
+    }
+
+    private function getGuesser($defaultLocale = 'en', $allowedLocales = array('en', 'fr', 'de'))
     {
         $guesser = new BrowserLocaleGuesser($defaultLocale, $allowedLocales);
 
         return $guesser;
     }
 
-    private function getRequestWithBrowserPreferences($locale = 'en')
+    private function getRequestWithBrowserPreferences()
     {
         $request = Request::create('/');
-        $request->headers->set('Accept-language', 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4');
+        $request->headers->set('Accept-language', 'fr-FR,fr;q=0.1,en-US;q=0.6,en;q=0.4');
 
         return $request;
     }
 
-    private function getRequestWithEmptyBrowserPreferences($locale = 'en')
+    private function getRequestWithBrowserPreferencesMultipleLangLocales()
+    {
+        $request = Request::create('/');
+        $request->headers->set('Accept-language', 'fr-CH,fr;q=0.8,fr-FR;q=0.7, en-GB;q=0.6,en;q=0.5');
+
+        return $request;
+    }
+
+    private function getRequestWithEmptyBrowserPreferences()
     {
         $request = Request::create('/');
         $request->headers->set('Accept-language', '');
-
-        return $request;
-    }
-
-    private function getRequestWithSessionLocale()
-    {
-        $session = new Session(new MockArraySessionStorage());
-        $session->set('lunetics_locale', 'ru');
-        $request = Request::create('/');
-        $request->setSession($session);
-        $request->headers->set('Accept-language', 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4');
 
         return $request;
     }
