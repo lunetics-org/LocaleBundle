7ec38f1b4197c050231087c4b784c8cb67a6de7a
Matthias Breddin <mb@lunetics.com>
2012-10-21 23:19:26 +0200

    Fix

--- LocaleGuesser/CookieLocaleGuesser.php
+++ LocaleGuesser/CookieLocaleGuesser.php
@@ -56,7 +56,9 @@ class CookieLocaleGuesser implements LocaleGuesserInterface
      */
     public function guessLocale(Request $request)
     {
-        if ($request->cookies->has($this->localeCookieName) && $this->metaValidator->isAllowed($request->cookies->get($this->localeCookieName))) {
+        if ($request->cookies->has($this->localeCookieName)
+                && $this->metaValidator->isAllowed($request->cookies->get($this->localeCookieName))
+        ) {
             $this->identifiedLocale = $request->cookies->get($this->localeCookieName);
 
             return true;
