12e00712458bf5d27286cbaddd7f77feb822d1a3
Asmir Mustafic <asmir@lignano.it>
2014-01-08 15:49:02 +0100

    Session test

---
+++ Tests/DependencyInjection/Compiler/RemoveSessionPassTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * This file is part of the LuneticsLocaleBundle package.
+ *
+ * <https://github.com/lunetics/LocaleBundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that is distributed with this source code.
+ */
+namespace Lunetics\LocaleBundle\Tests\DependencyInjection\Compiler;
+
+use Lunetics\LocaleBundle\DependencyInjection\Compiler\GuesserCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Lunetics\LocaleBundle\DependencyInjection\Compiler\RemoveSessionPass;
+
+/**
+ * @author Asmir Mustafic <goetas@gmail.com>
+ */
+class RemoveSessionPassTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @return \Symfony\Component\DependencyInjection\ContainerBuilder
+     */
+    private function getContainer(){
+        $container = new ContainerBuilder();
+
+        $container->register('lunetics_locale.session_guesser');
+        $container->register('lunetics_locale.locale_session');
+        return $container;
+    }
+    public function testSessionPresent()
+    {
+        $container = $this->getContainer();
+
+        $container->register('session');
+
+        $this->process($container);
+
+        $this->assertTrue($container->hasDefinition('lunetics_locale.session_guesser'));
+        $this->assertTrue($container->hasDefinition('lunetics_locale.locale_session'));
+
+    }
+    public function testSessioAbsent()
+    {
+        $container = $this->getContainer();
+
+        $this->process($container);
+
+        $this->assertFalse($container->hasDefinition('lunetics_locale.session_guesser'));
+        $this->assertFalse($container->hasDefinition('lunetics_locale.locale_session'));
+
+    }
+
+    protected function process(ContainerBuilder $container)
+    {
+        $pass = new RemoveSessionPass();
+        $pass->process($container);
+    }
+}
