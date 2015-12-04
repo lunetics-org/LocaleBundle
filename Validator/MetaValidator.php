6f5da9b7d6283d46505736d6884963dea5bdb766
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2015-12-04 15:19:55 +0100

    bug #176 dropped support for legacy validation api

--- Validator/MetaValidator.php
+++ Validator/MetaValidator.php
@@ -10,9 +10,6 @@
 namespace Lunetics\LocaleBundle\Validator;
 
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Validator\ValidatorInterface as LegacyValidatorInterface;
-use Lunetics\LocaleBundle\Validator\Locale;
-use Lunetics\LocaleBundle\Validator\LocaleAllowed;
 
 /**
  * This Metavalidator uses the LocaleAllowed and Locale validators for checks inside a guesser
@@ -20,21 +17,17 @@ use Lunetics\LocaleBundle\Validator\LocaleAllowed;
 class MetaValidator
 {
     /**
-     * @var Validator
+     * @var ValidatorInterface
      */
     private $validator;
 
     /**
      * Constructor
      *
-     * @param ValidatorInterface|LegacyValidatorInterface $validator
+     * @param ValidatorInterface $validator
      */
-    public function __construct($validator)
+    public function __construct(ValidatorInterface $validator)
     {
-        if (!$validator instanceof ValidatorInterface && !$validator instanceof LegacyValidatorInterface) {
-            throw new \InvalidArgumentException('MetadataValidator accepts either the new or the old ValidatorInterface, '.get_class($validator).' was injected instead.');
-        }
-        
         $this->validator = $validator;
     }
 
@@ -47,14 +40,8 @@ class MetaValidator
      */
     public function isAllowed($locale)
     {
-        if ($this->validator instanceof ValidatorInterface) {
-            $errorListLocale = $this->validator->validate($locale, new Locale);
-            $errorListLocaleAllowed = $this->validator->validate($locale, new LocaleAllowed);
-        } else {
-            // for Symfony <2.5 compatibility
-            $errorListLocale = $this->validator->validateValue($locale, new Locale);
-            $errorListLocaleAllowed = $this->validator->validateValue($locale, new LocaleAllowed);
-        }
+        $errorListLocale = $this->validator->validate($locale, new Locale);
+        $errorListLocaleAllowed = $this->validator->validate($locale, new LocaleAllowed);
 
         return (count($errorListLocale) == 0 && count($errorListLocaleAllowed) == 0);
     }
